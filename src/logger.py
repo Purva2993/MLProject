6da5c6800450de98fc81debf5237485e7f57648e
Purva2993 <purwa.mugdiya@gmail.com>
2025-07-14 13:43:17 -0700

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -13,7 +13,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-'''
-if __name__ == "__main__":
-    logging.info("Logger initialized successfully.")
-'''
+
+#if __name__ == "__main__":
+#    logging.info("Logger initialized successfully.")
