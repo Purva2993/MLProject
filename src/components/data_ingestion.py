eb1108b88099dadb168338637d0e9ab8d8cb995c
Purva2993 <purwa.mugdiya@gmail.com>
2025-07-14 16:20:01 -0700

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,7 +7,12 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass #Used to create clas variables
+
 from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer   
 
 
 @dataclass #using this decorator to create a class with variables, we do not need to write __init__ method 
@@ -49,4 +54,7 @@ if __name__ == "__main__":
      train_data, test_data = obj.initiate_data_ingestion()
 
      data_transformation = DataTransformation()
-     data_transformation.initiate_data_transformation(train_data, test_data)
+     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+     modeltrainer = ModelTrainer()
+     print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
