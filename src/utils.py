0c396e74d185afdf419de6581764feb317ee619d
Purva2993 <purwa.mugdiya@gmail.com>
2025-07-14 17:46:32 -0700

    PredictPipeline and Flask App

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pickle
 
 import pandas as pd
 import numpy as np
@@ -55,6 +56,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             models[model_name] = best_model
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
